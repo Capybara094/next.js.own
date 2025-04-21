71f9d3a6b8049a8a5a12d648852d16708a70876a
Capybara <xuh244693@gmail.com>
2025-04-21 22:56:50 +0800

    更改圖片存在位置

--- help/app/components/OuteCard.tsx
+++ help/app/components/OuteCard.tsx
@@ -7,13 +7,13 @@ type OuterCardProps = {
 export default function OutercardCard({ title, description, imageUrl }: OuterCardProps) {
   return (
     <div className="p-4 border rounded-lg shadow-md bg-gray-800">
-      {imageUrl && (
+      <div>
+        <h2 className="font-semibold text-xl text-white">{title}</h2>
+        {imageUrl && (
         <div className="mb-4">
           <img src={imageUrl} alt={title} className="w-full h-auto rounded-lg" />
         </div>
       )}
-      <div>
-        <h2 className="font-semibold text-xl text-white">{title}</h2>
         <p className="text-gray-300">{description}</p>
       </div>
     </div>
